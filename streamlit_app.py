8e211660357faa9ea4e01973c076d0564268a21d
takhan <tauseefak93@gmail.com>
2024-12-31 17:56:33 +0000

    creating vector dict

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,69 @@
 import streamlit as st
+import os
 from openai import OpenAI
 import pandas as pd
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+from streamlit_chromadb_connection.chromadb_connection import ChromadbConnection
+import boto3
+from io import BytesIO
+from dotenv import load_dotenv
+import docx2txt
+
+#strip non body text from emails
+def strip_emails(emails, openai_api_key):
+    # Create an OpenAI client.
+    client = OpenAI(api_key=openai_api_key)
+    body_text_list = []
+    for email in emails:
+        messages=[
+            {"role": "developer", "content": f"The following is the full text of an email extracted from a Word or PDF document, return only the body text of the email. Full Text: {email}"}
+            ]
+        # Use OpenAI's API to generate a response
+        response = client.chat.completions.create(
+            model="gpt-4o",  # Use GPT-4 model
+            messages=messages,
+            temperature=0.7,  # Control randomness in responses
+            n=1,  # Number of completions
+            stop=None,  # Let the model decide when to stop
+        )
+        body_text = response.choices[0].message.content
+        body_text_list.append(body_text)
+    return body_text_list
+
+def vectorize(emails):
+    #Add body text of emails to vector db
+    #collection = chroma_client.create_collection(name="test_collection")
+    configuration = {
+        "client": "PersistentClient",
+        "path": "./chromaclient/"
+    }
+    collection_name = "test_collection"
+    conn = st.connection("chromadb",
+                        type=ChromadbConnection,
+                        **configuration)
+    conn.create_collection(
+        collection_name=collection_name
+    )
+    ids = []
+    i=0
+    for email in emails:
+        ids.append("id"+str(i))
+        i+=1
+        conn.upload_documents(
+            collection_name=collection_name,
+            documents = emails,
+            ids=ids
+        )
+
+# Create S3 client
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+)
+
 
 # Show title and description.
 st.title("📄 Document question answering")
@@ -13,12 +76,33 @@ st.write(
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 openai_api_key = st.text_input("OpenAI API Key", type="password")
+
+if st.button("Get Data"):
+    if openai_api_key:
+        emails = []
+        # Store bucket name
+        bucket_name = "takhan-sample-email-bucket"
+
+        # Store contents of bucket
+        objects_list = s3.list_objects_v2(Bucket=bucket_name).get("Contents")
+
+        # Iterate over every object in bucket
+        for obj in objects_list:
+            #print(obj)
+            obj_name = obj["Key"]
+            response = s3.get_object(Bucket=bucket_name, Key=obj_name)
+            file_stream = BytesIO(response['Body'].read())
+            text= docx2txt.process(file_stream)
+            print(text)
+            emails.append(text)
+        body_text = strip_emails(emails, openai_api_key)
 #if not openai_api_key:
     #st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 #else:
 
 # Create an OpenAI client.
 client = OpenAI(api_key=openai_api_key)
+
 
 # Let the user upload a file via `st.file_uploader`.
 uploaded_file = st.file_uploader(
