607b767f0ac86d246daa496535eaaf28e69c2309
takhan <tauseefak93@gmail.com>
2025-01-01 20:01:12 +0000

    Added email drafting function

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,10 +21,13 @@ if 'collection_name' not in st.session_state:
 if 'chroma_client' not in st.session_state:
     st.session_state.chroma_client = chromadb.PersistentClient('./chromaclient/')
 
+if 'openai_api_key' not in st.session_state:
+    st.session_state.openai_api_key = os.getenv("OPENAI_KEY")
+
 #strip non body text from emails
-def strip_emails(emails, openai_api_key):
+def strip_emails(emails):
     # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+    client = OpenAI(api_key=st.session_state.openai_api_key)
     body_text_list = []
     for email in emails:
         messages=[
@@ -94,6 +97,56 @@ def find_docs(search_string):
     print(results)
     return results['documents'][0][0]
 
+def draft_email(email_string, context):
+    client = OpenAI(api_key=st.session_state.openai_api_key)
+    #key details prompt
+    details_prompt = f"You are a communications consultant working for a bank. You are using a previously sent sample email from a previous project as a template for writing a similar email. Use the following sample email to extract all of the important pieces of information communicated to the customer. Sample email: {email_string}"
+    #tone prompt
+    tone_prompt = f"You are a communications consultant working for a bank. You are using a previously sent sample email from a previous project as a template for writing a similar email. Use the following sample email to determine the tone to use in the new email. Describe the tone in one descriptive sentence. Sample email: {email_string}"
+    #sample key details retrieval
+    messages=[
+        {"role": "developer", "content": details_prompt}
+        ]
+    # Use OpenAI's API to generate a response
+    response = client.chat.completions.create(
+        model="gpt-4o",  # Use GPT-4 model
+        messages=messages,
+        temperature=0.7,  # Control randomness in responses
+        n=1,  # Number of completions
+        stop=None,  # Let the model decide when to stop
+    )
+    key_details = response.choices[0].message.content
+    print(key_details)
+    #sample tone retrieval
+    messages=[
+        {"role": "developer", "content": tone_prompt}
+        ]
+    # Use OpenAI's API to generate a response
+    response = client.chat.completions.create(
+        model="gpt-4o",  # Use GPT-4 model
+        messages=messages,
+        temperature=0.7,  # Control randomness in responses
+        n=1,  # Number of completions
+        stop=None,  # Let the model decide when to stop
+    )
+    tone = response.choices[0].message.content
+    print(tone)
+    prompt = f"You are a communications consultant working for a bank called BMO that has recently acquired Bank of the West. Draft the body of an email the bank is sending to its customers using the provided key copy points, details to include, and tone. The details to include and tone information come from a previous email about a similar subject, while the key copy points are specific to this project. The information in the key copy points should take precedence. Do not include any salutations, only draft the body of the email. \n Key Copy Points: {context}.\n Details to Include: {key_details}\n Tone to Use: {tone}"
+    messages=[
+    {"role": "developer", "content": prompt}
+    ]
+    # Use OpenAI's API to generate a response
+    response = client.chat.completions.create(
+        model="gpt-4o",  # Use GPT-4 model
+        messages=messages,
+        temperature=0.7,  # Control randomness in responses
+        n=1,  # Number of completions
+        stop=None,  # Let the model decide when to stop
+    )
+
+    # Extract and return the generated email text
+    return response.choices[0].message.content
+
 # Create S3 client
 s3 = boto3.client(
     "s3",
@@ -108,44 +161,30 @@ st.write(
     "Upload a tactical communications plan below and pick which email from the plan you would like to draft! "
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-
 if st.button("Get Data"):
-    if openai_api_key:
-        emails = []
-        # Store bucket name
-        bucket_name = "takhan-sample-email-bucket"
+    emails = []
+    # Store bucket name
+    bucket_name = "takhan-sample-email-bucket"
 
-        # Store contents of bucket
-        objects_list = s3.list_objects_v2(Bucket=bucket_name).get("Contents")
+    # Store contents of bucket
+    objects_list = s3.list_objects_v2(Bucket=bucket_name).get("Contents")
 
-        # Iterate over every object in bucket
-        for obj in objects_list:
-            #print(obj)
-            obj_name = obj["Key"]
-            response = s3.get_object(Bucket=bucket_name, Key=obj_name)
-            file_stream = BytesIO(response['Body'].read())
-            text= docx2txt.process(file_stream)
-            print(text)
-            emails.append(text)
-        body_text = strip_emails(emails, openai_api_key)
-        st.session_state.email_data = body_text
-        print("Session State Set!")
+    # Iterate over every object in bucket
+    for obj in objects_list:
+        #print(obj)
+        obj_name = obj["Key"]
+        response = s3.get_object(Bucket=bucket_name, Key=obj_name)
+        file_stream = BytesIO(response['Body'].read())
+        text= docx2txt.process(file_stream)
+        print(text)
+        emails.append(text)
+    body_text = strip_emails(emails)
+    st.session_state.email_data = body_text
+    print("Session State Set!")
 
 if st.button("Create Vector DB"):
     if st.session_state.email_data is not None:
         vectorize(st.session_state.email_data)
-
-#if not openai_api_key:
-    #st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-#else:
-
-# Create an OpenAI client.
-client = OpenAI(api_key=openai_api_key)
-
 
 # Let the user upload a file via `st.file_uploader`.
 uploaded_file = st.file_uploader(
@@ -163,10 +202,12 @@ if uploaded_file is not None:
         print(value)
         # Ask the user to edit the email description via `st.text_area`.
         question = st.text_area(
-            "Now ask a question about the document!",
+            "Edit the key copy points to include",
             value=value,
             placeholder="Can you give me a short summary?",
             disabled=not uploaded_file,
         )
         sample_email = find_docs(value)
-        st.write(sample_email)
+        if st.button("Draft Email"):
+            draft = draft_email(sample_email, value)
+            st.write(draft)
